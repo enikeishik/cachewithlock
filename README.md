0d21955c2babbd136666bca7e9f0a781f2aa17e1
enikeishik <enikeishik@gmail.com>
2021-09-13 14:21:58 +0300

    feat: init repository

---
+++ README.md
@@ -0,0 +1,29 @@
+# CacheWithLock
+
+Package for [Laravel framework](https://laravel.com/) - 
+overrides remember method of Laravel Cache using lock mechanism.
+
+## Requirements
+
+*   PHP >= 7.4
+*   Laravel >= 7.0
+
+## Install
+
+Install (or update) package via [composer](http://getcomposer.org/):
+
+```bash
+composer require enikeishik/cachewithlock
+```
+
+Make sure autoload will be changed:
+
+```bash
+composer dump-autoload
+```
+
+## Usage
+
+```php
+
+```
---
+++ src/CacheWithLock.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * CacheWithLock package for Laravel framework.
+ * 
+ * @copyright   Copyright (C) 2021 Enikeishik <enikeishik@gmail.com>. All rights reserved.
+ * @author      Enikeishik <enikeishik@gmail.com>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+declare(strict_types=1);
+
+namespace Enikeishik\CacheWithLock;
+
+use Closure;
+use Illuminate\Cache\CacheManager as BaseCache;
+
+/**
+ * This class overrides remember method using lock mechanism 
+ * to avoid multiply generation of the same data 
+ * (as a result of race condition) when cache becomes invalid
+ */
+class CacheWithLock extends BaseCache
+{
+    protected int $lockTimeout = 10;
+    
+    public function setLockTimeout(int $lockTimeout): void
+    {
+        $this->lockTimeout = $lockTimeout;
+    }
+    
+    public function getLockTimeout(): int
+    {
+        return $this->lockTimeout;
+    }
+    
+    public function remember(string $key, $ttl, Closure $callback)
+    {
+        $value = parent::get($key);
+        if (null !== $value) {
+            return $value;
+        }
+        
+        $lock = parent::lock($key . '_lock', $this->lockTimeout);
+        try {
+            if ($lock->block($this->lockTimeout)) {
+                //the main idea of this method is
+                //if cache already generated in another thread
+                //just get it and return without generation
+                $value = parent::get($key);
+                if (null !== $value) {
+                    return $value;
+                }
+                
+                $value = $callback();
+            }
+        } finally {
+            $lock->release();
+        }
+        
+        if (null !== $value) {
+            parent::put($key, $value, $ttl);
+        }
+        
+        return $value;
+    }
+}
