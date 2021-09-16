c706569136fd44b58d68ba2fe33b335e21c4d782
enikeishik <enikeishik@gmail.com>
2021-09-16 11:04:22 +0300

    feat: added extending standatr laravel Cache via service provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -11,14 +11,19 @@ declare(strict_types=1);
 
 namespace Enikeishik\CacheWithLock;
 
-use Illuminate\Support\ServiceProvider as ServiceProvider;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use Enikeishik\CacheWithLock\CacheManager;
+use Illuminate\Cache\CacheManager as BaseCacheManager;
 
-class ServiceProvider extends ServiceProvider
+class ServiceProvider extends BaseServiceProvider
 {
     public function register()
     {
-        $this->app->bind('Enikeishik\CacheWithLock\CacheManager', function($app) {
+        $this->app->singleton('Enikeishik\CacheWithLock\CacheManager', function($app) {
+            return new CacheManager($app);
+        });
+        
+        $this->app->extend(BaseCacheManager::class, function ($service, $app) {
             return new CacheManager($app);
         });
     }
